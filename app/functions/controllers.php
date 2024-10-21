c633f1f521b9cf5c737f448bdb150f1dbd8a069f
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-21 11:17:04 +0300

    controllers errors &  photo

--- app/functions/controllers.php
+++ app/functions/controllers.php
@@ -1,15 +1,50 @@
 <?php
-function home()
+/**
+ * transferring data to the page and displaying HOME
+ * @return void
+ */
+function home() : void
 {
-//    $names = getNames();
-//    $girlsSize = count($names['girl']);
-//    $boysSize = count($names['boy']);
-//    $size = max($girlsSize, $boysSize);
-//    $errors = getErrors();
-    render('home');
-//        , [
-//        'names' => $names,
-//        'size' => $size,
-//        'errors' => $errors
-//    ]);
+    $dataSource = getPhotos();
+    $errors = getErrors();
+    render('home'
+        , [
+            'dataSource' => $dataSource,
+            'errors' => $errors
+    ]);
 }
+
+
+/**
+ * photo reading, validation and saving
+ * Redirect to /index.php
+ * @return void
+ */
+function process() : void
+{
+    $errors = [];
+    $photos = [];
+    if(empty($_FILES['photo'])){
+        $errors[] = 'No POST data for file';
+    }else{
+        $photoFile = $_FILES['photo'];
+        $photos = diverseArray($photoFile);
+        $errors = validatePhoto($photos);
+    }
+    $res = false;
+    foreach($errors as $key => $error){
+        if (!empty($error)){
+            $res = true;
+        }
+    }
+    if (!$res) {
+        setPhotos($photos,$errors);
+        if (!empty($errors)) {
+            setErrors($errors);
+        }
+    } else {
+        setErrors($errors);
+    }
+    redirect('/index.php');// To index.php
+}
+
