3eaefc1dc078ec30054e240109438446298e0209
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-21 10:13:10 +0300

    add controllers

---
+++ app/functions/controllers.php
@@ -0,0 +1,15 @@
+<?php
+function home()
+{
+//    $names = getNames();
+//    $girlsSize = count($names['girl']);
+//    $boysSize = count($names['boy']);
+//    $size = max($girlsSize, $boysSize);
+//    $errors = getErrors();
+    render('home');
+//        , [
+//        'names' => $names,
+//        'size' => $size,
+//        'errors' => $errors
+//    ]);
+}
