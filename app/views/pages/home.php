40f31791c1715e9c2a6011690898ab8ecedbdbad
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-21 11:16:16 +0300

    form home view errors & photo

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -5,3 +5,31 @@
         <button type="submit">Upload</button>
     </div>
 </form>
+<?php if (!empty($errors)): ?>
+    <ul>
+        <?php foreach ($errors as $key => $error):
+            if (!empty($error)): ?>
+                <li><?=  ERROR_NAME[$key]; ?></li>
+                <ul>
+                    <?php foreach ($error as $item): ?>
+                        <li><?= $item; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+<?php if(!empty($dataSource)) : ?>
+    <div class="pswp-gallery pswp-gallery--single-column" id="mygallery">
+        <?php if (count($dataSource) > 0) :?>
+            <?php foreach ($dataSource as $source) : ?>
+                <a href="<?= $source['src']?>"
+                   data-pswp-width="<?= $source['width']?>"
+                   data-pswp-height="<?= $source['height']?>"
+                   target="_blank">
+                    <img src="<?= $source['src']?>" alt="<?= $source['alt']?>" />
+                </a>
+            <?php endforeach;?>
+        <?php endif;?>
+    </div>
+<?php endif;?>
